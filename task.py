3edc0ac35a11d20ff7e2c53af69ced328b9331ca
kemal332 <80928103+kemal332@users.noreply.github.com>
2021-04-22 14:17:30 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,54 +1,112 @@
-#for checking
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
-deposite= input("how much that you want deposite")
-takingmoney=input("how much that you want to take money?")
 
-class thebank :
-  def __init__(self,name,surname,age):
-    self.name = name
-    self.surname= surname 
-    self.age= age
+import random
 
-  def sayhello(self):
-    print("welcome our bank {name} ".format(name=self.name))
 
-class customer (thebank):
-  def __init__(self,name,age):
-    self.name =name
-    self.age=age 
-    self.remainder=100
-  def what_is_remainder(self):
-    return self.remainder
-  def todeposite(self):
-    self.remainder += deposite
-  def takemoney(self):
-    self.remainder -=takingmoney
+class bank():
+    def __init__(self,name="kemal",surname="cetinakay",password=1234,balance=7900,thebankname="bnp",banksnames=["ing","bnp","ziraat"]):
+        self.name=name
+        self.surname=surname
+        self.password=password
+        self.balance=balance
+        self.banksnames=banksnames
+        self.thebankname=thebankname
+    def money(self):
+        while True:
+            answer=input("if you want to withdrawal please press'-'for deposite press '+'")
+            if (answer== '-'):
+                if (self.balance<=5):
+                    break
+                else:
+                    value=int(input("the value you want to withdrawal"))
+                    self.balance-=value
+            
+            elif(answer=='+'):
+                value=int(input("the value you want to deposite"))
+                self.balance+=value
+            
+            else:
+                break
+    
+    def changingbank(self):
+        randomly = random.randint(0,len(self.banksnames)-1)
+        self.thebankname=self.banksnames(randomly)
+    
 
-customer1 = customer("kemal",23)
-print(customer1.sayhello())
-print(customer1.what_is_remainder())
+class customer(bank):
+        def __init__(self,name,surname,credit,password,balance,thebankname="bnp",banksnames=["ing","bnp","ziraat"]):
+            super().__init__(name="kemal",surname="cetinakay",password=1234,balance=7900,thebankname="",banksnames=["ing","bnp","ziraat"])
+            self.credit=credit
 
+        def discharge(self):
+            answer2=input("if you want to pay your kredit press 'pay'; if you want some extra credit press'yes' ")
+            while True:
+                if (answer2=='pay'):
+                    moneyvalue=int(input("write your payment"))
+                    self.credit-=moneyvalue
+                    return self.credit #?
+                
+                elif(answer2=='yes'):
+                    if (self.kredit<=-2000):
+                        break
+                    else:
+                        moneyvalue=int(input("write your payment"))
+                        self.credit+=moneyvalue
+                        return self.credit # ?
+                
+                else:
+                    break
+                    
+        def moneytransfer(self):
+            transferred=int(input("how much that you want to sent "))
+            self.balance-=transferred
+            
+        def __str__(self):
+            return "name:{}\nsurname:{}\npassword:{}\nbalance:{}\nthebankname:{}\ncredit:{} ".format(self.name,self.surname,self.password,self.balance,self.thebankname,self.credit)
+        
+
+            
+customer1 =customer("kemal","cetinkaya",7800,13213,7000)                
+print("""
+
+1)deposit and  withdrawal
+
+2) changing bank
+
+3)credit transactions
+
+4)money transfer
+
+5) information
+
+6) delete account
+
+getting out press 'q'
+""")
+
+while True:
+    process=input("enter your process")
+    if(process=='q'):
+        break
+    elif(process=='1'):
+        customer1.money()
+        
+    elif(process=='2'):
+        customer1.changingbank()
+    elif(process=='3'):
+        customer1.discharge()
+    elif(process=='4'):
+        customer1.moneytransfer()
+    elif(process=='5'):
+        print(customer1)
+    elif(process=='6'):
+        del(customer1)
+        print("your account is deleted")
+    else:
+        print("unused digit")
+
+                    
+                
+        
+            
+            
+        
